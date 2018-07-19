b4b879ace8f859959db683c2025cf28fe83ac93e
Andrew Mackenzie <33694902+smartlyMac@users.noreply.github.com>
2018-07-19 12:52:38 +0100

    Add BookContract.java

---
+++ app/src/main/java/com/example/android/bookstore/data/BookContract.java
@@ -0,0 +1,19 @@
+package com.example.android.bookstore.data;
+
+import android.provider.BaseColumns;
+
+public class BookContract {
+
+    public static final class BookEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "books";
+        public static final String _ID = BaseColumns._ID;
+        public static final String PRODUCT_NAME = "Product Name";
+        public static final String PRICE = "Price";
+        public static final String QTY = "Qty.";
+        public static final String SUPPLIER_NAME = "Supplier Name";
+        public static final String SUPPLIER_TEL = "Supplier Tel.";
+
+    }
+
+}
