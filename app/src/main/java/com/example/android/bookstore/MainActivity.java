ec367f8308f605a22e35ff988d579658df7de01a
Andrew Mackenzie <33694902+smartlyMac@users.noreply.github.com>
2018-07-25 15:44:09 +0100

    Refactor MainActivity.java to utilise Content Provider

--- app/src/main/java/com/example/android/bookstore/MainActivity.java
+++ app/src/main/java/com/example/android/bookstore/MainActivity.java
@@ -2,18 +2,13 @@ package com.example.android.bookstore;
 
 import android.content.ContentValues;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import com.example.android.bookstore.data.BookContract.BookEntry;
 import com.example.android.bookstore.data.BookDbHelper;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,30 +28,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 insertData();
                 displayDbInfo();
-                queryData();
             }
         });
-
         displayDbInfo();
-        queryData();
-
     }
 
     private void displayDbInfo() {
 
-        // Create/open a database to read from it
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
         // Get a cursor that returns all rows in the books table
-        Cursor cursor = db.query(
-                BookEntry.TABLE_NAME,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null);
+        Cursor cursor = getContentResolver().query(BookEntry.CONTENT_URI, null, null, null, null);
 
         TextView mainTextView = findViewById(R.id.text_view_main);
 
@@ -73,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
 
             while (cursor.moveToNext()) {
                 int currentID = cursor.getInt(cursor.getColumnIndex(BookEntry._ID));
-                String currentProduct = cursor.getString(cursor.getColumnIndex(BookEntry.PRODUCT_TITLE));
+                String currentTitle = cursor.getString(cursor.getColumnIndex(BookEntry.PRODUCT_TITLE));
+                String currentAuthor = cursor.getString(cursor.getColumnIndex(BookEntry.PRODUCT_AUTHOR));
                 int currentPrice = cursor.getInt(cursor.getColumnIndex(BookEntry.PRICE)) / 100;
                 int currentQty = cursor.getInt(cursor.getColumnIndex(BookEntry.QTY));
                 String currentSupplier = cursor.getString(cursor.getColumnIndex(BookEntry.SUPPLIER_NAME));
@@ -81,7 +62,8 @@ public class MainActivity extends AppCompatActivity {
 
                 mainTextView.append("\n"
                         + currentID + " - "
-                        + currentProduct + " - "
+                        + currentTitle + " - "
+                        + currentAuthor + " - "
                         + "£" + currentPrice + " - "
                         + currentQty + " - "
                         + currentSupplier + " - "
@@ -98,9 +80,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void insertData() {
 
-        // Access a writable version of the database
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
         // Create the content values for the new book
         ContentValues values = new ContentValues();
         values.put(BookEntry.PRODUCT_TITLE, "A Universe From Nothing");
@@ -110,35 +89,8 @@ public class MainActivity extends AppCompatActivity {
         values.put(BookEntry.SUPPLIER_TEL, "01234567890");
 
         // Add the new book to the database
-        db.insert(BookEntry.TABLE_NAME, null, values);
+        getContentResolver().insert(BookEntry.CONTENT_URI, values);
 
     }
 
-    private void queryData() {
-
-        // Access a readable version of the database
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        // Query the database to display only ID, Product Name, and Quantity
-        String[] columns = {BookEntry._ID, BookEntry.PRODUCT_TITLE, BookEntry.QTY};
-        String[] args = {"3"};
-        Cursor cursor = db.query(
-                BookEntry.TABLE_NAME,
-                columns,
-                BookEntry._ID + "=?",
-                args,
-                null,
-                null,
-                null,
-                null);
-
-        int columnCount = cursor.getColumnCount();
-        int rowCount = cursor.getCount();
-
-        TextView queryTextView = findViewById(R.id.text_view_query);
-
-        queryTextView.setText("Query Response: No. Columns: " + columnCount + ", No. Rows: " + rowCount);
-
-        cursor.close();
-    }
 }
