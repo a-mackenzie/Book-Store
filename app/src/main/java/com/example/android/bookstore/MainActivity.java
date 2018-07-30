cdecd6e8e979bb6e876766fb7df5ec5adf8d9d74
Andrew Mackenzie <33694902+smartlyMac@users.noreply.github.com>
2018-07-30 14:40:29 +0100

    Add menu to MainActivity and move 'insertData' method to here via Insert Dummy Book menu option Remove 'Add' button from activity_main.xml

--- app/src/main/java/com/example/android/bookstore/MainActivity.java
+++ app/src/main/java/com/example/android/bookstore/MainActivity.java
@@ -10,6 +10,8 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -33,15 +35,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mDbHelper = new BookDbHelper(this);
-
-        // Set onClickListener for add dummy book button
-        Button dummyBookButton = findViewById(R.id.fab);
-        dummyBookButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                insertData();
-            }
-        });
 
         mCursorAdapter = new BookCursorAdapter(this, null);
 
@@ -99,5 +92,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         mCursorAdapter.changeCursor(null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.insert_dummy_book:
+                insertData();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
