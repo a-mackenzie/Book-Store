26fd408ff354151852c165568b411f964c9b0e8a
Andrew Mackenzie <33694902+smartlyMac@users.noreply.github.com>
2018-07-30 15:08:50 +0100

    Add FAB to activity_main.xml

--- app/src/main/java/com/example/android/bookstore/EditorActivity.java
+++ app/src/main/java/com/example/android/bookstore/EditorActivity.java
@@ -2,13 +2,10 @@ package com.example.android.bookstore;
 
 import android.app.LoaderManager;
 import android.content.CursorLoader;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
