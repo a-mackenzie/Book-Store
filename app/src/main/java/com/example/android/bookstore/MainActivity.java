a9ad4f62c2d05e667c162cc7e8211c36a1fdbb08
Andrew Mackenzie <33694902+smartlyMac@users.noreply.github.com>
2018-08-03 21:17:08 +0100

    Add Delete All option to MainActivity menu

--- app/src/main/java/com/example/android/bookstore/MainActivity.java
+++ app/src/main/java/com/example/android/bookstore/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.bookstore;
 
+import android.app.AlertDialog;
 import android.app.LoaderManager;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
@@ -19,6 +21,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.android.bookstore.data.BookContract.BookEntry;
 import com.example.android.bookstore.data.BookDbHelper;
 
@@ -120,7 +124,43 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             case R.id.insert_dummy_book:
                 insertData();
                 return true;
+            case R.id.delete_all:
+                showDeleteAllConfirmationDialog();
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showDeleteAllConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.dialog_delete_all_confirm);
+        builder.setPositiveButton(R.string.dialog_yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                deleteAll();
+            }
+        });
+        builder.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                if (dialog != null) {
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void deleteAll() {
+        String toastMessage;
+        int deleteInt = getContentResolver().delete(BookEntry.CONTENT_URI, null, null);
+        if (deleteInt == 0) {
+            toastMessage = getString(R.string.toast_delete_all_error);
+        } else {
+            toastMessage = getString(R.string.toast_delete_all_success);
+        }
+
+        Toast toast = Toast.makeText(getBaseContext(), toastMessage, Toast.LENGTH_LONG);
+        toast.show();
     }
 }
